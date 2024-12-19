7b054a734a6c989fea0b4d4ca5f993b457a3c971
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-12-19 00:06:00 +0000

    pushing to heroku

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,13 +1,22 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 if os.path.exists("env.py"):
     import env  # noqa
 
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# Add an if statement to conditionally set the database URI
+if os.environ.get("DB_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    database_url = os.environ.get("DATABASE_URL")
+    # Adjust the URL if it starts with "postgres://"
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 
 db = SQLAlchemy(app)
 
